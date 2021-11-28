e8f8df51d3cc6309a217680f0a3443d09d67250f
Frank <Frank@DESKTOP-FA8EJCS>
2021-11-28 11:09:23 +0800

    add test

--- Refactor/RefactorTests/CustomerTests.cs
+++ Refactor/RefactorTests/CustomerTests.cs
@@ -25,20 +25,70 @@ namespace Refactor.Tests
     public class CustomerTests
     {
         [TestMethod()]
-        public void statementTest()
+        public void statementTest_1()
         {
             // Arrange
             var customer = new Customer("Frank");
-            var retalList = new List<RentalParameter>()
+            var parameterList = new List<RentalParameter>()
             {
                 new RentalParameter("StartWars",Movie.NEW_RELEASE,3),
                 new RentalParameter("Avatar",Movie.CHILDRENS,1),
                 new RentalParameter("Matrixs",Movie.REGULAR,5),
                 new RentalParameter("KongFu",Movie.NEW_RELEASE,3),
             };
-            retalList.ForEach(x => customer.addRental(ToRental(x)));
+            parameterList.ForEach(x => customer.addRental(ToRental(x)));
 
             var expexted = "Rental Record for Frank\n\tStartWars\t9\n\tAvatar\t1.5\n\tMatrixs\t6.5\n\tKongFu\t9\nAmount owed is 26\nYou earned 6 frequent renter popints";
+            // Act
+            var actual = customer.statement();
+
+            // Assert
+            Assert.AreEqual(actual, expexted);
+        }
+
+        [TestMethod()]
+        public void statementTest_2()
+        {
+            // Arrange
+            var customer = new Customer("Frank");
+            var parameterList = new List<RentalParameter>()
+            {
+                new RentalParameter("StartWars",Movie.NEW_RELEASE,3),
+                new RentalParameter("StartWars2",Movie.NEW_RELEASE,4),
+                new RentalParameter("Catch Me if You Can",Movie.NEW_RELEASE,5),
+                new RentalParameter("Avatar",Movie.CHILDRENS,1),
+                new RentalParameter("Matrixs",Movie.REGULAR,5),
+                new RentalParameter("KongFu",Movie.NEW_RELEASE,3),
+            };
+            parameterList.ForEach(x => customer.addRental(ToRental(x)));
+
+            var expexted = "Rental Record for Frank\n\tStartWars\t9\n\tStartWars2\t12\n\tCatch Me if You Can\t15\n\tAvatar\t1.5\n\tMatrixs\t6.5\n\tKongFu\t9\nAmount owed is 53\nYou earned 10 frequent renter popints";
+
+            // Act
+            var actual = customer.statement();
+
+            // Assert
+            Assert.AreEqual(actual, expexted);
+        }
+
+        [TestMethod()]
+        public void statementTest_3()
+        {
+            // Arrange
+            var customer = new Customer("Tina");
+            var parameterList = new List<RentalParameter>()
+            {
+                new RentalParameter("Stay",Movie.NEW_RELEASE,1),
+                new RentalParameter("Butterfly Effect",Movie.NEW_RELEASE,1),
+                new RentalParameter("Catch Me if You Can",Movie.NEW_RELEASE,1),
+                new RentalParameter("Avatar",Movie.CHILDRENS,1),
+                new RentalParameter("Matrixs",Movie.REGULAR,1),
+                new RentalParameter("KongFu",Movie.CHILDRENS,1),
+            };
+            parameterList.ForEach(x => customer.addRental(ToRental(x)));
+
+            var expexted = "Rental Record for Tina\n\tStay\t3\n\tButterfly Effect\t3\n\tCatch Me if You Can\t3\n\tAvatar\t1.5\n\tMatrixs\t2\n\tKongFu\t1.5\nAmount owed is 14\nYou earned 6 frequent renter popints";
+
             // Act
             var actual = customer.statement();
 
